95d8fe460cc53ce57f4fab82a564d5c91d407504
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2019-02-18 21:58:42 +0900

    Add message sending feature to slack

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/m-mizutani/sns-to-slack
 require (
 	github.com/aws/aws-lambda-go v1.8.2
 	github.com/aws/aws-sdk-go v1.16.36 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.3.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"encoding/json"
+
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,12 +23,64 @@ func main() {
 
 }
 
+type slackMessage struct {
+	Attachments []slackAttachment `json:"attachments,omitempty"`
+	Text        string            `json:"text"`
+	IconEmoji   string            `json:"icon_emoji,omitempty"`
+	UserName    string            `json:"username,omitempty"`
+}
+
+type slackAttachment struct {
+	Actions        []slackAction `json:"actions,omitempty"`
+	AttachmentType string        `json:"attachment_type,omitempty"`
+	AuthorIcon     string        `json:"author_icon,omitempty"`
+	AuthorName     string        `json:"author_name,omitempty"`
+	CallbackID     string        `json:"callback_id,omitempty"`
+	Color          string        `json:"color,omitempty"`
+	Fallback       string        `json:"fallback,omitempty"`
+	Fields         []slackField  `json:"fields,omitempty"`
+	ImageURL       string        `json:"image_url,omitempty"`
+	Text           string        `json:"text,omitempty"`
+	Title          string        `json:"title,omitempty"`
+}
+
+type slackAction struct {
+	Name  string `json:"name,omitempty"`
+	Text  string `json:"text,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type slackField struct {
+	Short bool   `json:"short,omitempty"`
+	Title string `json:"title,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
 func handler(ctx context.Context, snsEvent events.SNSEvent) error {
 	logger.WithField("event", snsEvent).Info("Start handler")
 
 	incomingURL := os.Getenv("WEBHOOK_URL")
 	if incomingURL == "" {
 		return errors.New("WEBHOOK_URL is required")
+	}
+
+	for _, record := range snsEvent.Records {
+		logger.WithField("record", record).Info("Start record")
+
+		var msg slackMessage
+		msg.Text = record.SNS.Message
+		buf, err := json.Marshal(msg)
+		if err != nil {
+			return errors.Wrap(err, "Fail to marshal slack message")
+		}
+
+		resp, err := http.Post(incomingURL, "application/json", bytes.NewReader(buf))
+		if err != nil {
+			return errors.Wrap(err, "Fail to post message")
+		}
+
+		logger.WithField("resp", resp).Debug("done HTTP request")
 	}
 
 	logger.Info("Exit handler")
